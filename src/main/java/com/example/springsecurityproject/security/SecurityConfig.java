ad5ee8cd5db40c87f3976a87942b4554e67855ec
roncohen-dev <ron.cohen.job@gmail.com>
2023-06-19 08:32:17 +0300

    feat: form based authentication to login/logout

--- src/main/java/com/example/springsecurityproject/security/SecurityConfig.java
+++ src/main/java/com/example/springsecurityproject/security/SecurityConfig.java
@@ -14,6 +14,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableWebSecurity
@@ -26,16 +29,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-//                .csrf().disable()
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .and()
+                .csrf().disable()
+//                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                .and()
                 .authorizeRequests()
-                .antMatchers("/","/swagger-ui/**", "index.html", "css/*", "js/*").permitAll()
+                .antMatchers("/", "/swagger-ui/**", "index.html", "css/*", "js/*").permitAll()
                 .antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .defaultSuccessUrl("/courses", true)
+                .passwordParameter("password")
+                .usernameParameter("username")
+                .and()
+                .rememberMe().tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21)).key("notDefaultPasswordOfSpringSecurity")
+                .rememberMeParameter("remember").and().logout().logoutUrl("/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET")) // this lane just if .csrf().disable()
+                .clearAuthentication(true)
+                .invalidateHttpSession(true).deleteCookies("JSESSIONID", "XSRF-TOKEN")
+                .logoutSuccessUrl("/login");
     }
 
     @Override
